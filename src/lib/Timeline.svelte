eee4859442058eab0e3a2114fcd86797da2d8957
Eugen Volosciuc <volosciuc.eugen@gmail.com>
2024-02-02 07:20:54 +0200

    feat: added day columns

--- src/lib/Timeline.svelte
+++ src/lib/Timeline.svelte
@@ -4,15 +4,20 @@
 
 	import * as TogglService from '@/services/toggl';
 	import type { Task } from '@/types';
+	import { getDaysBetween, checkIsWeekend } from '@/utils/date-utils';
 
 	let tasks: Task | null = null;
 	let error: any = null;
 
+	const startDate = dayjs().subtract(1, 'week');
+	const endDate = dayjs().add(1, 'week');
+	const dateRange = getDaysBetween(startDate, endDate);
+
 	onMount(async () => {
 		const { workspaceId, teamId } = TogglService.getRequestData();
 		const { data, error: tasksError } = await TogglService.getTasks({
-			since: dayjs().subtract(1, 'week').toDate(),
-			until: dayjs().add(1, 'week').toDate(),
+			since: startDate.toDate(),
+			until: endDate.toDate(),
 			teamId,
 			workspaceId,
 		});
@@ -22,27 +27,30 @@
 	});
 </script>
 
-<style lang="postcss">
-	.logo {
-		color: #8b55dd;
-		display: block;
-		flex: none;
-		background: currentColor;
-		mask-size: contain;
-		mask-repeat: no-repeat;
-		mask-position: center;
-		width: 1.8333ex;
-		height: 1.8333ex;
-		mask-image: url('../assets/logo-symbol.svg');
-	}
-</style>
+<main class="flex min-h-screen items-stretch justify-center">
+	<!-- TODO: save the tasks in a context provider -->
+	<!-- Step 1: Make the calendar -->
+	{#each dateRange as date, i}
+		{@const isToday = date.isToday()}
+		{@const isWeekend = checkIsWeekend(date)}
 
-<main class="flex min-h-screen items-center justify-center">
-	<a
-		class="logo"
-		href="https://plan.toggl.com"
-		target="_blank"
-		rel="noopener noreferrer">
-	</a>
-	<p class="ml-1">👋 Welcome to your homework template!</p>
+		<div class="flex flex-col text-purple-950" class:bg-slate-100={isWeekend}>
+			<!-- Header -->
+			<div class="border-b-[1px] border-b-slate-200 bg-white p-1">
+				<p
+					class="rounded px-4 py-1 text-sm"
+					class:bg-orange-500={isToday}
+					class:text-white={isToday}
+					class:text-slate-400={isWeekend}>
+					{date.format('dd')[0]}
+					{date.format('M')}
+				</p>
+			</div>
+			<!-- Timeline -->
+			<div
+				class="flex-1 border-r-[1px] border-b-slate-200"
+				class:border-l-[1px]={i === 0}>
+			</div>
+		</div>
+	{/each}
 </main>
---
+++ src/utils/date-utils.ts
@@ -0,0 +1,19 @@
+import dayjs, { Dayjs } from 'dayjs';
+import isToday from 'dayjs/plugin/isToday';
+
+dayjs.extend(isToday);
+
+export const getDaysBetween = (start: Dayjs, end: Dayjs) => {
+	const range = [];
+	let current = start;
+	while (!current.isAfter(end)) {
+		range.push(current);
+		current = current.add(1, 'days');
+	}
+	return range;
+};
+
+export const checkIsWeekend = (date: Dayjs) => {
+	const dayOfTheWeek = dayjs(date).day();
+	return dayOfTheWeek === 0 || dayOfTheWeek === 6;
+};
