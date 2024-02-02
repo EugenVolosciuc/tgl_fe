9e13fed2ea0af0aad3052598a89cf953133c1cd4
Eugen Volosciuc <volosciuc.eugen@gmail.com>
2024-02-02 06:17:08 +0200

    chore: added src path alias, installed dayjs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src/'),
+		},
+	},
+
 	plugins: [svelte()],
 });
