94bba4d441e61c9f606d802b172804963e606bdd
Eugen Volosciuc <volosciuc.eugen@gmail.com>
2024-02-03 16:16:35 +0200

    feat: further work with svelte-dnd-action

--- src/utils/stores/task-drag-store.ts
+++
@@ -1,7 +0,0 @@
-import type { Task } from '@/types';
-import { writable } from 'svelte/store';
-
-export const taskDragStore = writable<{
-	draggedTask: Task | null;
-	hoveringOverDate: string | null;
-}>({ draggedTask: null, hoveringOverDate: null });
